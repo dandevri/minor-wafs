188b68775c69d4c23d9284b1371980ae23876569
Danny de Vries <vriesdan@gmail.com>
2017-02-09 16:50:56 +0100

    Added Web App from Scratch

--- week1/assignment-5/static/app.js
+++ week1/assignment-5/static/app.js
@@ -1,17 +1,38 @@
-var app = {
-  init: function() {
+(function() {
 
-  }
-};
+//Object Literal
+  var app = {
+    init: function() {
+      routes.init();
+      sections.init();
+    }
+  };
 
-var routes = {
-  init: function() {
+  // Elk object heeft eigen onderdeel ter verantwoordelijkheid.
+  var routes = {
+    init: function() {
+      window.addEventListener('hashchange', function() {
+          sections.toggle(window.location.hash);
+      });
+    }
+  };
 
-  }
-};
+  var sections = {
+    init: function() {
+      this.toggle('#home');
+    },
+    toggle: function(elementSelector) {
 
-var sections = {
-  toggle: function(route) {
+      // Alles op hidden
+      var sectionElements = document.querySelectorAll('section');
+      for(var i = 0; i < sectionElements.length; i++) {
+        sectionElements[i].classList.add('hidden');
+      }
+      // Eentje van hidden af
+      document.querySelector(elementSelector).classList.remove('hidden');
+    }
+  };
 
-  }
-};
+  app.init();
+
+})();
